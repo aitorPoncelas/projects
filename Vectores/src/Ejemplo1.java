24cef3e68ddbe75531c542f9e35aafe4928efb9a
aitorPoncelas <aitor.rmd@gmail.com>
2016-05-17 13:39:16 +0200

    Primera prueba

---
+++ Vectores/src/Ejemplo1.java
@@ -0,0 +1,22 @@
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []v={14,15,1,2,3};
+		int []w= v.clone();
+		int []z=v;
+		System.out.println("z[0]= "+z[0]);
+		
+		for(int i=0;i<w.length;i++)
+			System.out.println("w["+i+"]= "+w[i]);
+		v[0]=1500;
+				
+		
+		System.out.println("w[0]= "+w[0]);
+		System.out.println("z[0]= "+z[0]);
+		if(v.equals(z)) System.out.println("z Igual a v");
+		if(v.equals(w)) System.out.println("w Igual a v");
+	}
+
+}
